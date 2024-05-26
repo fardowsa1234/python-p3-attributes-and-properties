ded4da09323c64bf345112f945f8e000f263b001
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-05-26 21:13:18 +0300

    python

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+
+    def __init__(self, name="Unknown", breed="Unknown"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            print(f"Setting name to {name}.")
+            self._name = name
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        print("Retrieving breed.")
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed not in Dog.approved_breeds:
+            print("Breed must be in list of approved breeds.")
+        else:
+            print(f"Setting breed to {breed}.")
+            self._breed = breed
+
+    breed = property(get_breed, set_breed)
+
+
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,38 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+# lib/person.py
 class Person:
-    pass
+    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            print(f"Setting name to {name}.")
+            self._name = name.title()
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        print("Retrieving job.")
+        return self._job
+
+    def set_job(self, job):
+        if job not in Person.approved_jobs:
+            print("Job must be in list of approved jobs.")
+        else:
+            print(f"Setting job to {job}.")
+            self._job = job
+
+    job = property(get_job, set_job)
+
+
